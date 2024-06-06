5e34dd69fee7514f4d655296e6ef8bd495ed7699
Norman Qian <normanqian@gmail.com>
2024-06-05 23:17:48 -0400

    heatmap start

--- src/App.tsx
+++ src/App.tsx
@@ -16,33 +16,3 @@ function App() {
 }
 
 export default App;
-
-/*
-const fakedata: messageThread = {
-  messages: [
-    { content: "firstmessage", id: 0 },
-    { content: "second message here", id: 1 },
-    {
-      content:
-        "lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor ametlorem ipsum sit dolor amet lorem ipsum sit dolor amet",
-      id: 2,
-    },
-    {
-      content:
-        "lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor ametlorem ipsum sit dolor amet lorem ipsum sit dolor amet",
-      id: 2,
-    },
-    {
-      content:
-        "lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor ametlorem ipsum sit dolor amet lorem ipsum sit dolor amet",
-      id: 2,
-    },
-    {
-      content:
-        "lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor ametlorem ipsum sit dolor amet lorem ipsum sit dolor amet",
-      id: 2,
-    },
-  ],
-  isOutgoing: true,
-};
-*/
